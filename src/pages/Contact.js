f7e25ca6f0d9eace11a5d330bff85c4050fa4c3e
Salinamalek <salinamalek@pursuit.org>
2024-07-03 18:36:16 -0400

    links fixed

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,10 +6,14 @@ export default function Contact() {
       <div>
         <h2>Feel Free to Contact Me at:</h2>
         <h3>
-          <a href="https://www.linkedin.com/in/salina-malek/">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/salina-malek/" target="_blank">
+            LinkedIn
+          </a>
         </h3>
         <h3>
-          <a href="https://github.com/Salinamalek">GitHub</a>
+          <a href="https://github.com/Salinamalek" target="_blank">
+            GitHub
+          </a>
         </h3>
         <h3>
           <a href="mailto:salinamalek@pursuit.org">Email</a>
